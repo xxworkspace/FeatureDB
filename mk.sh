005e727052e2c3cdba63ebc73d3be77d088f4e7e
sunli <sunli@bigo.sg>
2019-06-18 14:56:46 +0800

    Update mk.sh and .cpp

--- mk.sh
+++ mk.sh
@@ -1,5 +1,38 @@
-mkdir lib
-g++ -O3 -shared -std=c++11 -fPIC -I./ -I./hnswlib src/FeatureDB.cpp -o lib/libfeaturedb.so
+echo "cd HierarchicalNSW/gemv"
+cd HierarchicalNSW/gemv
+echo "mkdir bin"
+mkdir bin
+echo "g++ -std=c++11 codegen/codegen_gemv_25_10.cpp -o bin/codegen_gemv_25_10"
+g++ -std=c++11 codegen/codegen_gemv_25_10.cpp -o bin/codegen_gemv_25_10
+echo "g++ -std=c++11 codegen/codegen_kernel.cpp -o bin/codegen_kernel"
+g++ -std=c++11 codegen/codegen_kernel.cpp -o bin/codegen_kernel
+echo "./bin/codegen_gemv_25_10"
+./bin/codegen_gemv_25_10
+echo "./bin/codegen_kernel"
+./bin/codegen_kernel
+echo "mv GemvKernel.h ./include"
+mv GemvKernel.h ./include
+echo "mv AVXKernel.h ./include"
+mv AVXKernel.h ./include
+echo "mkdir obj"
+mkdir ../../obj
+echo "compile *.o"
+g++ -std=c++11 -masm=intel -fPIC  -I./include -c GemvKernel.cc -o ../../obj/GemvKernel.o
+g++ -std=c++11 -masm=intel -fPIC  -I./include -c AVXKernel.cc -o  ../../obj/AVXKernel.o
+g++ -std=c++11 -fPIC -I./include -c src/cpuinfo.cc -o ../../obj/cpuinfo.o
+g++ -std=c++11 -fPIC -I./include -c src/kernel.cc -o  ../../obj/kernel.o
+g++ -std=c++11 -fPIC -I./include -c src/gemv.cc -o  ../../obj/gemv.o
+g++ -std=c++11 -fPIC -I./include -c src/int8.cc -o  ../../obj/int8.o
+g++ -std=c++11 -fPIC -I./include -c src/util.cpp -o ../../obj/util.o
+echo "compile *.so"
+cd ../../
+g++ -O3 -shared -std=c++11 -fPIC -I./ -I./HierarchicalNSW -I./HierarchicalNSW/gemv/include src/FeatureDB.cpp \
+obj/GemvKernel.o obj/AVXKernel.o obj/cpuinfo.o obj/kernel.o obj/gemv.o obj/int8.o obj/util.o -o lib/libfeaturedb.so
+echo "cleaning ..."
+rm -r obj
+rm -r HierarchicalNSW/gemv/bin
+rm HierarchicalNSW/gemv/*.cc
+rm HierarchicalNSW/gemv/include/*Kernel*
 mkdir bin
 g++ -std=c++11 -I./ -L./lib test/test.cpp -o bin/test -lfeaturedb -fopenmp
 cp test/*fvecs bin
--- src/FeatureDB.cpp
+++ src/FeatureDB.cpp
@@ -1,10 +1,10 @@
 
 #include "FeatureDB.h"
-#include "hnswlib/hnswalg.h"
+#include "HierarchicalNSW/hnsw.h"
 
 namespace bigo::ml {
 
-  typedef hnswlib::HierarchicalNSW<float> INDEX;
+  typedef hnsw::HierarchicalNSW<half,half,float> INDEX;
   template<class T>
   FeatureDB<T>::FeatureDB(
     const std::string space_name,
@@ -17,16 +17,17 @@ namespace bigo::ml {
     :Dim(dim) {
     normalize = false;
     initialize = false;
-    hnswlib::SpaceInterface<float> *space;
-    if (space_name == "ip") space = new hnswlib::InnerProductSpace(dim);
-    else if (space_name == "l2") space = new hnswlib::L2Space(dim);
+    gemv::DisT dist;
+    if (space_name == "ip") dist = gemv::DisT::IP;
+    else if (space_name == "l2") dist = gemv::DisT::L2;
     else if (space_name == "cosine") {
-      space = new hnswlib::InnerProductSpace(dim);
+      dist = gemv::DisT::IP;
       normalize = true;
     }
-
-    hnsw = (void*)new INDEX(space, max_elements, M, construction_ef);
-    ((INDEX*)hnsw)->setEf(query_ef);
+    if(std::is_same<T,float>::value)
+      hnsw = (void*)new INDEX(max_elements, M, dim, query_ef, construction_ef, dist);
+    else
+      printf("Only float is supported!");
   }
 
   template<class T>
@@ -35,145 +36,103 @@ namespace bigo::ml {
   }
 
   template<class T>
-  void FeatureDB<T>::normalization(const T* src, T* dst,const unsigned dim) {
-    const T* tmp = src;
-    T sum = 0;
-    for (int i = 0; i < dim; ++i) {
-      sum += (*tmp)*(*tmp);
-      ++tmp;
-    }
-    sum = sqrt(sum);
-    for (int i = 0; i < dim; ++i)
-      *(dst++) = *(src++) / sum;
-  }
+  void FeatureDB<T>::normalization(const T* src, T* dst,const unsigned dim) {}
 
   template<class T>
   unsigned FeatureDB<T>::size() {
-    return ((INDEX*)hnsw)->cur_element_count;
+    return ((INDEX*)hnsw)->size();
   }
 
   template<class T>
   const std::vector<char> FeatureDB<T>::dump() {
-    std::vector<char> serial;
-    ((INDEX*)hnsw)->saveIndex(serial);
-    return serial;
+    return ((INDEX*)hnsw)->dump();
   }
 
   template<class T>
   bool FeatureDB<T>::load(const std::vector<char>& serial) {
     if (initialize) return false;
     initialize = true;
-    ((INDEX*)hnsw)->loadIndex(serial);
+    ((INDEX*)hnsw)->load(serial);
     return true;
   }
 
-#define CHECK_DIM(obj, src, rt) \
+#define CHECK_EQ(obj, src, rt) \
     if (obj != src) return rt;
+
+#define __ToHalf__                                          \
+  void* tmp = aligned_alloc(64,sizeof(float)*Dim);          \
+  memcpy(tmp,&data[0],sizeof(float)*Dim);                   \
+  gemv::KParams<float,half> kp;                             \
+  kp.dim = Dim;                                             \
+  kp.src.push_back((float*)tmp);                            \
+  kp.dst.push_back((half*)tmp);                             \
+  if(normalize)                                             \
+    L2NormAVX(kp);                                          \
+  else                                                      \
+    Float2HalfAVX(kp);
 
   template<class T>
   bool FeatureDB<T>::insert(const std::vector<T>& data,const uint64_t label) {
     initialize = true;
-    CHECK_DIM(data.size(), Dim, false)
-      if (normalize) {
-        float *tmp = new float[Dim];
-        normalization(&data[0], tmp, Dim);
-        ((INDEX*)hnsw)->addPoint(tmp, label);
-        delete tmp;
-      }
-      else
-        ((INDEX*)hnsw)->addPoint(&data[0], label);
+    CHECK_EQ(data.size(), Dim, false)
+
+    __ToHalf__
+    ((INDEX*)hnsw)->insertPoint((half*)tmp, label);
+    free(tmp);
     return true;
   }
 
   template<class T>
   std::vector<std::pair<float, uint64_t>> FeatureDB<T>::query(const std::vector<T>& data,const unsigned k) {
     std::vector<std::pair<float, uint64_t>> result;
-    CHECK_DIM(data.size(), Dim, result)
-      if (normalize) {
-        float *tmp = new float[Dim];
-        normalization(&data[0], tmp, Dim);
-        auto top_candidate = ((INDEX*)hnsw)->searchKnn(tmp, k);
-		result.resize(top_candidate.size());
-        unsigned count = top_candidate.size();
-        for (int i = count - 1; i >= 0; --i) {
-          result[i] = top_candidate.top();
-          top_candidate.pop();
-        }
-        delete tmp;
-      }
-      else {
-        auto top_candidate = ((INDEX*)hnsw)->searchKnn(&data[0], k);
-        result.resize(top_candidate.size());
-        unsigned count = top_candidate.size();
-        for (int i = count - 1; i >= 0; --i) {
-          result[i] = top_candidate.top();
-          top_candidate.pop();
-        }
-      }
+    CHECK_EQ(data.size(), Dim, result)
+
+    __ToHalf__
+    auto top_candidate = ((INDEX*)hnsw)->searchKnn((half*)tmp, k);
+    result.resize(top_candidate.size());
+    unsigned count = top_candidate.size();
+    for (int i = count - 1; i >= 0; --i) {
+      result[i] = top_candidate.top();
+      top_candidate.pop();
+    }
+    free(tmp);
     return result;
   }
 
   template<class T>
   std::vector<std::pair<float, uint64_t>> FeatureDB<T>::queryAndInsert(const std::vector<T>& data,const uint64_t label,const unsigned k) {
     std::vector<std::pair<float, uint64_t>> result;
-    CHECK_DIM(data.size(), Dim, result)
+    CHECK_EQ(data.size(), Dim, result)
 
-      if (normalize) {
-        float* tmp = new float[Dim];
-        normalization(&data[0], tmp, Dim);
-        auto top_candidate = ((INDEX*)hnsw)->searchKnn(tmp, k);
-        result.resize(top_candidate.size());
-        unsigned count = top_candidate.size();
-        for (int i = count - 1; i >= 0; --i) {
-          result[i] = top_candidate.top();
-          top_candidate.pop();
-        }
-        ((INDEX*)hnsw)->addPoint(tmp, label);
-        delete tmp;
-      }
-      else {
-        auto top_candidate = ((INDEX*)hnsw)->searchKnn(&data[0], k);
-        result.resize(top_candidate.size());
-        unsigned count = top_candidate.size();
-        for (int i = count - 1; i >= 0; --i) {
-          result[i] = top_candidate.top();
-          top_candidate.pop();
-        }
-        ((INDEX*)hnsw)->addPoint(&data[0], label);
-      }
+    __ToHalf__
+    auto top_candidate = ((INDEX*)hnsw)->searchKnn((half*)tmp, k);
+    result.resize(top_candidate.size());
+    unsigned count = top_candidate.size();
+    for (int i = count - 1; i >= 0; --i) {
+      result[i] = top_candidate.top();
+      top_candidate.pop();
+    }
+    ((INDEX*)hnsw)->insertPoint((half*)tmp, label);
+    free(tmp);
     return result;
   }
 
   template<class T>
   std::vector<std::pair<float, uint64_t>> FeatureDB<T>::queryAndInsert(const std::vector<T>& data,const uint64_t label,const float threshold,const unsigned k) {
     std::vector<std::pair<float, uint64_t>> result;
-    CHECK_DIM(data.size(), Dim, result)
+    CHECK_EQ(data.size(), Dim, result)
 
-      if (normalize) {
-        float* tmp = new float[Dim];
-        normalization(&data[0], tmp, Dim);
-        auto top_candidate = ((INDEX*)hnsw)->searchKnn(tmp, k);
-        result.resize(top_candidate.size());
-        unsigned count = top_candidate.size();
-        for (int i = count - 1; i >= 0; --i) {
-          result[i] = top_candidate.top();
-          top_candidate.pop();
-        }
-        if (result[0].first > threshold)
-          ((INDEX*)hnsw)->addPoint(tmp, label);
-        delete tmp;
-      }
-      else {
-        auto top_candidate = ((INDEX*)hnsw)->searchKnn(&data[0], k);
-        result.resize(top_candidate.size());
-        unsigned count = top_candidate.size();
-        for (int i = count - 1; i >= 0; --i) {
-          result[i] = top_candidate.top();
-          top_candidate.pop();
-        }
-        if (result[0].first > threshold)
-          ((INDEX*)hnsw)->addPoint(&data[0], label);
-      }
+    __ToHalf__
+    auto top_candidate = ((INDEX*)hnsw)->searchKnn((half*)tmp, k);
+    result.resize(top_candidate.size());
+    unsigned count = top_candidate.size();
+    for (int i = count - 1; i >= 0; --i) {
+      result[i] = top_candidate.top();
+      top_candidate.pop();
+    }
+    if (result[0].first > threshold)
+      ((INDEX*)hnsw)->insertPoint((half*)tmp, label);
+    free(tmp);
     return result;
   }
 
